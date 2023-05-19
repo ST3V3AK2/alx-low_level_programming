2c190a33834764a26df2eb59ab2e449089f775ea
Stephen <stephenayuba16@gmail.com>
2023-05-18 21:05:27 -0700

    Basics of C Trst 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s[62] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, s, 62);
+	return (0);
+}
