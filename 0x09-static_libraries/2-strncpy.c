b09bcf34b7674fbece0f7f6ccdd4da6808cc1372
Stephen <stephenayuba16@gmail.com>
2023-06-09 21:44:53 -0700

    Create script for creating libraries

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -9,17 +9,4 @@
  * Return: a pointer to the dest srting
  */
 
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i = 0;;
-	int n = sizeof(dest) - 1;
-
-	while (i < n && src[i] != '\0')
-	{
-		*(dest  + i) = *(src + i);
-		i++;
-	}
-	for ( ; i < n; i++)
-		dest{i} = '\0';
-	return (dest);
-}
+char *_strncpy(char *dest, char *src, int n);
