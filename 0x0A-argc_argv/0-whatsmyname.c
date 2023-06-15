f2ed519432f47d7252cb20315f6993e4a4175222
Stephen <stephenayuba16@gmail.com>
2023-06-14 19:02:00 -0700

    Correct minor errors in C script

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,6 +12,8 @@
 
 int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", *argv);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,8 +14,11 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc <= 1)
+	if (argc != 3)
+	{
 		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
 	return (0);
