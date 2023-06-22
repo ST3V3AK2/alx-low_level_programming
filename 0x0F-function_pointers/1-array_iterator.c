d4fb2123baaa7cb6e2790c71ff40a096e8d50003
Stephen <stephenayuba16@gmail.com>
2023-06-22 05:57:59 +0100

    Create function pointers dirctory and c files

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: pointer to input array
+ * @size: pointer to siz of the  input array
+ * @action: input function
+ * 
+ * Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        action(array[i]);
+    }
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Test code
+ * @argc: number of arguments to main
+ * @agrv: string of arguments passed to main
+ *
+ * Return: ALways 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b; 
+	int result;
+	int (*operator)(int)(int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	if (a == NULL || b == NULL)
+	{
+		printf("Error\n");
+		return (-1)
+	}
+	operator = get_op_function(argv[2]);
+	result = (*operator)(a, b);
+	printf("%d", result\n);
+	return (0);
+}
