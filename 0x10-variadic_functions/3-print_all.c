ba20aac30bf57251145c50949a016d557b44d798
Steve <stephenayuba16@gmail.com>
2023-06-27 17:46:58 +0100

    Make corrections in 0x10-10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	char c, *str;
-	float f;
+	double f;
 	int n = sizeof(format) - 1;
 	int i = 0, num;
 
@@ -27,28 +27,22 @@ void print_all(const char * const format, ...)
 				c = va_arg(args, int);
 				printf("%c", c);
 				break;
-
 			case 'i':
 				num = va_arg(args, int);
 				printf("%d", num);
 				break;
-
 			case 'f':
 				f = va_arg(args, double);
 				printf("%f", f);
 				break;
-
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
 					printf("(nil)");
 				printf("%s", str);
 				break;
-
 			default:
 				continue;
-				break;
-
 		}
 		i++;
 	}
