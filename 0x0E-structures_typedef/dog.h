896ac143f5b0890cb52232281d822eb91b5dd017
Stephen <stephenayuba16@gmail.com>
2023-07-05 18:00:57 -0700

    Appeasing Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef HEADER_D
 #define HEADER_D
 
+/**
+ * struct dog - registers a dog
+ * @name: Dog's name
+ * @age: Dog's ag
+ * @owner: Dog's owner
+ *
+ * Description: briefly identifies a Dog.
+ */
+
 typedef struct dog
 {
-    char *name;
-    char *owner;
-    float age;
-}dog_t;
+	char *name;
+	char *owner;
+	float age;
+} dog_t;
 
 #endif
