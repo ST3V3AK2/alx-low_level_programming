be4829b07c1fd1ed1b465df1eacc4bff1954906c
ST3V3AK2 <128585174+ST3V3AK2@users.noreply.github.com>
2023-07-08 19:02:20 +0100

    Delete 0x0F-function_pointers directory

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER
-#define HEADER
-
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, int size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
