dde24b156076b1f4d21877ad22d2b465d7eeb3fe
Stephen <ayubastephen212@gmail.com>
2023-09-16 11:19:32 -0700

    Correct Structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,8 @@ typedef struct dog
 	char *name;
 	char *owner;
 	float age;
-}dog_t;
+} dog_t;
 
-struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
