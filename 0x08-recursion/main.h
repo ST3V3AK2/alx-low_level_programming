a92c5d7fba31fd45833cf789fe7d4fc7dd6944bb
Stephen <ayubastephen212@gmail.com>
2023-09-24 09:43:21 -0700

    Correct nam of files

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,6 +14,7 @@ int sqrt(int x, int y);
 int _sqrt_recursion(int n);
 int check_prime(int x, int y);
 int is_prime_number(int n);
+int p_checker(char *s, char *c, int i);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
