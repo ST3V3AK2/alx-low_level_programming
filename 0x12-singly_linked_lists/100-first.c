20da0d548319f14f9532ed59870c64d279961751
Stephen <ayubastephen212@gmail.com>
2023-09-26 13:05:18 -0700

    Complete code for advanced tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,9 @@
  * Return: Always 0.
  */
 
-int bootup(void)
+void print_something(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 	return (0);
 }
