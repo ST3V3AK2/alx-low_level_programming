1b2070124e36a4052aa02076cc25102b3b2fee8b
Stephen <ayubastephen212@gmail.com>
2023-10-31 06:47:53 -0700

    Fix minor problems

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,13 +19,13 @@ int rdwr2file(int file_from, int file_to, char **av)
 	num_rd = read(file_from, &buf, count);
 	if (errno == EACCES || num_rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	num_wr = write(file_to, &buf, num_rd);
 	if (errno == EACCES || num_wr == -1)
 	{
-		dprintf(2, "Error: Can't write to  file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to  file %s\n", av[2]);
 		exit(99);
 	}
 
@@ -54,6 +54,9 @@ int main(int ac, char *av[])
 	}
 
 	file_from = open(av[1], O_RDONLY);
+
+	if (file_to == NULL)
+		exit(1);
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	check = 0;
